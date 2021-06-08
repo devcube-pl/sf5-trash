9c7274e860d378be0b58a087641edbe9bf6e3ada
Jakub Łukasik <kuba@devcube.pl>
2021-06-08 09:21:28 +0200

    Reation between user and post

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -42,6 +42,12 @@ class Post
      */
     private $publishedAt;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="posts")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -103,6 +109,18 @@ class Post
     public function setPublishedAt(?\DateTimeInterface $publishedAt): self
     {
         $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
